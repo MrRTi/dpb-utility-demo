b6c52593988ec8d191be289861e3f2b6e9d465f4
Artem Musalitin <musalitin.a.a@gmail.com>
2022-01-16 15:59:40 +0000

    Add .byebug history into gitingnore

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -2,10 +2,5 @@
 
 module Web
   class ApplicationController < ApplicationController
-    rescue_from ActionController::RoutingError, with: :respond_not_found
-
-    def respond_not_found
-      head :not_found
-    end
   end
 end
